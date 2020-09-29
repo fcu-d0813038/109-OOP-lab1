57d8b743cb4e446ded933b32583b2800b581e407
user <user@EC03>
2020-09-29 09:36:32 +0800

    1

---
+++ src/hw1/hw1.java
@@ -0,0 +1,13 @@
+package hw1;
+
+public class hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm ªL«C½n");
+		System.out.println("My nid is D0813038");
+		System.out.println("This is lab 1 hw");
+
+	}
+
+}
